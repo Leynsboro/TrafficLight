3f064b5ccbc2362665726dedc97bd52fd8a81797
Ilya <leynsboro@gmail.com>
2022-04-07 22:52:24 +0300

    Constraints it's so shitty

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
 
+    @IBOutlet var redLight: UIView!
+    @IBOutlet var yellowLight: UIView!
+    @IBOutlet var greenLight: UIView!
+    @IBOutlet var startButtonOutlet: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLight.layer.cornerRadius = redLight.frame.width / 2
+        yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
+        greenLight.layer.cornerRadius = greenLight.frame.width / 2
+        startButtonOutlet.layer.cornerRadius = 10
     }
-
+    
+    
+    @IBAction func startButtonPressed() {
+    }
+    
+    
 
 }
 
