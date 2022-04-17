d21a138607efc28fb0d38fec8fabb48bc42a13bd
Ilya <leynsboro@gmail.com>
2022-04-17 17:06:25 +0300

    Fixes but for what?)

--- TrafficLight/ViewController.swift
+++ TrafficLight/ViewController.swift
@@ -7,27 +7,34 @@
 
 import UIKit
 
+enum Colors {
+    case red, yellow, green
+}
+
 class ViewController: UIViewController {
     
 
     @IBOutlet var redLight: UIView!
     @IBOutlet var yellowLight: UIView!
     @IBOutlet var greenLight: UIView!
-    @IBOutlet var startButtonOutlet: UIButton!
     
-    enum Colors {
-        case red, yellow, green
-    }
+    @IBOutlet var startButtonOutlet: UIButton!
     
     var color = Colors.red
     
-    
+    private let turnOn: CGFloat = 1
+    private let turnOff: CGFloat = 0.3
+        
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        startButtonOutlet.layer.cornerRadius = 10
+    }
+    
+    override func viewWillLayoutSubviews() {
         redLight.layer.cornerRadius = redLight.frame.width / 2
         yellowLight.layer.cornerRadius = yellowLight.frame.width / 2
         greenLight.layer.cornerRadius = greenLight.frame.width / 2
-        startButtonOutlet.layer.cornerRadius = 10
     }
     
     
@@ -36,22 +43,20 @@ class ViewController: UIViewController {
         
         switch color {
         case .red:
-            greenLight.alpha = 0.3
-            redLight.alpha = 1
-            color = .yellow
+            turn(on: redLight, andOf: greenLight, color: .yellow)
         case .yellow:
-            redLight.alpha = 0.3
-            yellowLight.alpha = 1
-            color = .green
+            turn(on: yellowLight, andOf: redLight, color: .green)
         case .green:
-            yellowLight.alpha = 0.3
-            greenLight.alpha = 1
-            color = .red
+            turn(on: greenLight, andOf: yellowLight, color: .red)
         }
-        
     }
-    
-    
+}
 
+extension ViewController {
+    private func turn(on lightOn: UIView, andOf lightOff: UIView, color: Colors) {
+        lightOn.alpha = turnOn
+        lightOff.alpha = turnOff
+        self.color = color
+    }
 }
 
